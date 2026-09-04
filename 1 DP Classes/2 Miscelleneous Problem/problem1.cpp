5b590dbfd5c2518cfdca8c63da7b5d50c1e753b7
Yashaswi Pathak <pathakyashaswi213@gmail.com>
2026-09-04 19:42:07 +0530

    Rename file

---
+++ 1 DP Classes/2 Miscelleneous Problem/problem1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sqrt=1,num,x,a=1,n;
+    cout<<"enter the base number";
+    cin>>x;
+    cout<<"enter the power value";
+    cin>>n;
+    while(a<=n)
+    {
+        sqrt =sqrt*x ;
+        a++;
+    }
+    cout<<sqrt;
+    return 0;
+}
